8ab543ad4b066cee2fe1893e46d68c2400db5710
nowamey <nowamey100@gmail.com>
2022-09-28 00:13:42 -0400

    fixed the SINGLE MISSING BRACKET

--- client/tetris/app.js
+++ client/tetris/app.js
@@ -16,12 +16,12 @@ document.addEventListener('DOMContentLoaded', () => {
   ]
 
     //The Tetrominoes
-      const lTetromino = [
-        [1, width+1, width*2+1, 2],
-        [width, width+1, width+2, width*2+2],
-        [1, width+1, width*2+1, width*2],
-        [width, width*2, width*2+1, width*2+2]
-      ]
+  const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+    ]
     
     const zTetromino = [
         [0,width,width+1,width*2+1],
@@ -71,7 +71,7 @@ document.addEventListener('DOMContentLoaded', () => {
       function undraw(){
         current.forEach(index => {
             squares[currentPosition + index].classList.remove('tetromino')
-            squares[currentPosition+index].style.backgroundColor = ''
+            squares[currentPosition+index].style.backgroundColor =''
         })
       }
 
@@ -101,19 +101,20 @@ document.addEventListener('DOMContentLoaded', () => {
         freeze()
       }
 
-      function freeze(){
-        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
-            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
-
-            random = nextRandom 
-            nextRandom = Math.floor(Math.random() *theTetrominoes.length)
-            current = theTetrominoes[random][currentRotation]
-            currentPosition = 4
-            draw()
-            displayShape()
-            addScore()
-            gameOver()
+      function freeze() {
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+          current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+          //start a new tetromino falling
+          random = nextRandom
+          nextRandom = Math.floor(Math.random() * theTetrominoes.length)
+          current = theTetrominoes[random][currentRotation]
+          currentPosition = 4
+          draw()
+          displayShape()
+          addScore()
+          gameOver()
         }
+      
       }
 
 
@@ -157,24 +158,27 @@ document.addEventListener('DOMContentLoaded', () => {
       }
       const displaySquares = document.querySelectorAll('.mini-grid div')
       const displayWidth = 4
-      let displayIndex = 0
+      const displayIndex = 0
 
       //the Tetrominos without rotations 
       const upNextTetrominoes = [
-        [1,displayWidth+1,displayWidth*2+1,2], //l
-        [0,displayWidth,displayWidth+1,displayWidth*2+1],//z
-        [1,displayWidth,displayWidth+1,displayWidth+2],//t
-        [0,1,displayWidth,displayWidth+1] //o
-        [1,displayWidth+1,displayWidth*2+1,displayWidth*3+1]//i
+        [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
+        [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
+        [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
+        [0, 1, displayWidth, displayWidth+1], //oTetromino
+        [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
       ]
+    
 
-      function displayShape(){
-        displaySquares.forEach(square=>{
+      function displayShape() {
+        //remove any trace of a tetromino form the entire grid
+        displaySquares.forEach(square => {
           square.classList.remove('tetromino')
+          square.style.backgroundColor = ''
         })
-        upNextTetrominoes[nextRandom].forEach(index =>{
-          displaySquares[displayIndex+index].classList.add('tetromino')
-          squares[currentPosition+index].style.backgroundColor = colors[nextRandom]
+        upNextTetrominoes[nextRandom].forEach( index => {
+          displaySquares[displayIndex + index].classList.add('tetromino')
+          displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom]
         })
       }
       startBtn.addEventListener('click', ()=> {
@@ -193,7 +197,7 @@ document.addEventListener('DOMContentLoaded', () => {
         for(let i =0;i<199;i+=width){
           const row = [i,i+1,i+2,i+3,i+4,i+5,i+6,i+7,i+8,i+9]
 
-          if(row.every(index => squares[index].classList.contains('taken')))
+          if(row.every(index => squares[index].classList.contains('taken'))){
           score+=10
           scoreDisplay.innerHTML = score
           row.forEach(index => {
@@ -205,6 +209,7 @@ document.addEventListener('DOMContentLoaded', () => {
           squares =squaresRemoved.concat(squares)
           squares.forEach(cell => grid.appendChild(cell))
         }
+      }
       }
       function gameOver(){
         if(current.some(index=> squares[currentPosition+index].classList.contains('taken'))){
